d90f2bc8ec218bc08d081328fc4b75d91f47ceab
Dalso13 <flskwhdhk@gmail.com>
2025-03-24 15:09:43 +0900

    feat: dialog를 통해 todo 작성

--- app/src/main/java/com/jdw/diet_memo/MainActivity.kt
+++ app/src/main/java/com/jdw/diet_memo/MainActivity.kt
@@ -1,12 +1,53 @@
 package com.jdw.diet_memo
 
+import android.app.DatePickerDialog
+import android.icu.util.Calendar
+import android.icu.util.GregorianCalendar
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.widget.Button
+import android.widget.DatePicker
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btn = findViewById<Button>(R.id.btn)
+
+        btn.setOnClickListener {
+            val dialog = LayoutInflater.from(this).inflate(R.layout.custom_dialog, null)
+            val builder = AlertDialog.Builder(this)
+                .setView(dialog)
+                .setTitle("운동 메모 다이얼로그")
+
+            val alertDialog = builder.show()
+
+            val dateBtn = alertDialog.findViewById<Button>(R.id.btn)
+
+            dateBtn?.setOnClickListener {
+
+                val today = GregorianCalendar()
+                val year : Int = today.get(Calendar.YEAR)
+                val month : Int = today.get(Calendar.MONTH)
+                val date : Int = today.get(Calendar.DATE)
+
+                val dlg = DatePickerDialog(this, object : DatePickerDialog.OnDateSetListener{
+                    override fun onDateSet(
+                        view: DatePicker?,
+                        year: Int,
+                        month: Int,
+                        dayOfMonth: Int
+                    ) {
+                        dateBtn.setText("${year}.${month}.${date}")
+                    }
+                }, year, month, date)
+
+                dlg.show()
+            }
+        }
 
     }
 }
