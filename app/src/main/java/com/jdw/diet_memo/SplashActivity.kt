ed55a0d807d3a5426ad81ec14e9c861bb8734a2f
Dalso13 <flskwhdhk@gmail.com>
2025-03-24 14:37:26 +0900

    feat: 익명 로그인 구현

--- app/src/main/java/com/jdw/diet_memo/SplashActivity.kt
+++ app/src/main/java/com/jdw/diet_memo/SplashActivity.kt
@@ -3,19 +3,49 @@ package com.jdw.diet_memo
 import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
-import androidx.activity.enableEdgeToEdge
+import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class SplashActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
-        Handler().postDelayed({
-            startActivity(Intent(this, MainActivity::class.java))
-            finish()
-        }, 3000)
+        auth = Firebase.auth
+
+        var currentUser = auth.currentUser
+
+        if (currentUser == null) {
+            auth.signInAnonymously()
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        // Sign in success, update UI with the signed-in user's information
+                        currentUser = auth.currentUser
+                        Handler().postDelayed({
+                            startActivity(Intent(this, MainActivity::class.java))
+                            finish()
+                        }, 3000)
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        Toast.makeText(
+                            baseContext,
+                            "Authentication failed.",
+                            Toast.LENGTH_SHORT,
+                        ).show()
+                    }
+                }
+        } else {
+            Handler().postDelayed({
+                startActivity(Intent(this, MainActivity::class.java))
+                finish()
+            }, 3000)
+        }
     }
 }
